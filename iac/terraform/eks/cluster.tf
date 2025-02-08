05a5a9047f8e76114e8b9949bece73da5240cb82
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2025-02-08 10:43:43 -0300

    Feat/refactor terraform files (#5)
    * Refactor terraform files
    
    * Refactor terraform files
    
    * Refactor terraform files
    
    * Create Action and fix provider

---
+++ iac/terraform/eks/cluster.tf
@@ -0,0 +1,38 @@
+resource "aws_eks_cluster" "techchalenge_cluster" {
+  name     = "techchalenge-eks-cluster"
+  role_arn = aws_iam_role.cluster_role.arn
+
+  vpc_config {
+    subnet_ids = data.terraform_remote_state.network.outputs.public_subnet_ids
+  }
+
+  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController
+  ]
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.techchalenge_cluster.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.techchalenge_cluster.certificate_authority[0].data
+}
+
+resource "aws_eks_node_group" "techchallenge_node_group" {
+  cluster_name    = aws_eks_cluster.techchalenge_cluster.name
+  node_group_name = "techchallenge-node-group"
+  node_role_arn   = aws_iam_role.node_group_role.arn
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+  subnet_ids     = data.terraform_remote_state.network.outputs.public_subnet_ids
+}
---
+++ iac/terraform/eks/iam.tf
@@ -0,0 +1,59 @@
+resource "aws_iam_role" "cluster_role" {
+  name = "techchalenge-cluster-role"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "eks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  role       = aws_iam_role.cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
+  role       = aws_iam_role.cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+
+resource "aws_iam_role" "node_group_role" {
+  name = "techchalenge-node-group-role"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_group_policy_eks" {
+  role       = aws_iam_role.node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_group_policy_ec2" {
+  role       = aws_iam_role.node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "node_group_policy_cni" {
+  role       = aws_iam_role.node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
